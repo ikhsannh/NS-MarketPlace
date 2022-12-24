5e0e75aa8be81c62a52a2c1937f1492ec4e44cc0
nashkispace <nashkispace@gmail.com>
2022-12-25 01:06:15 +0700

    fix media-query mobile

--- client-side/src/scenes/global/Footer.jsx
+++ client-side/src/scenes/global/Footer.jsx
@@ -1,8 +1,10 @@
 import { useTheme } from "@mui/material";
 import { Box, Typography } from "@mui/material";
 import { shades } from "../../theme";
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 const Footer = () => {
+    const isNonMobile = useMediaQuery("(min-width:600px)");
     const { palette: { neutral },
     } = useTheme();
     return (
@@ -11,47 +13,53 @@ const Footer = () => {
                 width="80%"
                 margin="auto"
                 display="flex"
-                justifyContent="space-between"
+                justifyContent="space-around"
                 flexWrap="wrap"
                 rowGap="30px"
-                columnGap="clamp(20px, 30px, 40px)"
+                columnGap="clamp(20px, 30px, 30px)"
             >
-                <Box width="clamp(20%, 30%, 40%)">
-                    <Typography variant="h4" fontWeight="bold" mb="30px" color={shades.primary}>NS - Market Place</Typography>
-                    <div>
+                <Box 
+                    width={isNonMobile ? "clamp(20%, 30%, 40%)" : "clamp(20%, 100%, 100%)" }>                
+                    <Typography variant="h3" fontWeight="bold" mb="10px" color={shades.primary}>NS - Market Place</Typography>
+                    <Typography ml={isNonMobile ? undefined : "5px"}> 
                     Auctor, erat elementum ab similique curabitur parturient! Nostrud. 
                     Habitant, libero! Nobis in aute placeat potenti ullam maiores conubia. 
                     Blandit, atque. Labore, aperiam! Ultrices curabitur natus consectetur excepturi, 
                     doloribus sociis unde nibh, massa laoreet, culpa justo! At netus bibendum. Qui facere.
-                    </div>
+                    </Typography>
                 </Box>
-              <Box> 
-                <Typography variant="h4" fontWeight="bold" mb="30px">
+              <Box 
+                 width={isNonMobile ? "clamp(20%, 10%, 40%)" : "clamp(100%, 25%, 30%)"}> 
+                <Typography variant="h3" fontWeight="bold" mb="10px">
                     About Us
                 </Typography>
-                <Typography mb="30px">Careers</Typography>
-                <Typography mb="30px">Our Stores</Typography>
-                <Typography mb="30px">Term & Conditions</Typography>
-                <Typography mb="30px">Privacy Policy</Typography>
+                <Typography  ml={isNonMobile ? undefined : "5px"} mb="5px">Careers</Typography>
+                <Typography  ml={isNonMobile ? undefined : "5px"} mb="5px">Our Stores</Typography>
+                <Typography  ml={isNonMobile ? undefined : "5px"} mb="5px">Term & Conditions</Typography>
+                <Typography  ml={isNonMobile ? undefined : "5px"} mb="5px">Privacy Policy</Typography>
               </Box>
 
-              <Box> 
-                <Typography variant="h4" fontWeight="bold" mb="30px">
+              <Box 
+                ml={isNonMobile ? "-10px" : "auto"} 
+                width={isNonMobile ? "clamp(20%, 10%, 40%)" : "clamp(100%, 25%, 30%)"}> 
+                <Typography variant="h3" fontWeight="bold" mb="10px">
                     Customer Care
                 </Typography>
-                <Typography mb="30px">Help Center</Typography>
-                <Typography mb="30px">Track Your Order</Typography>
-                <Typography mb="30px">Corporate & Purchasing</Typography>
-                <Typography mb="30px">Returns & Refunds</Typography>
+                <Typography  ml={isNonMobile ? undefined : "5px"} mb="5px">Help Center</Typography>
+                <Typography  ml={isNonMobile ? undefined : "5px"} mb="5px">Track Your Order</Typography>
+                <Typography  ml={isNonMobile ? undefined : "5px"} mb="5px">Corporate & Purchasing</Typography>
+                <Typography  ml={isNonMobile ? undefined : "5px"} mb="5px">Returns & Refunds</Typography>
               </Box>
 
-              <Box width="clamp(20%, 25%, 30%)">
-                 <Typography variant="h4" fontWeight="bold" mb="30px">
+              <Box 
+                ml={isNonMobile ? undefined : "-5px"}
+                width={isNonMobile ? "clamp(20%, 10%, 40%)" : "clamp(100%, 25%, 30%)"}>
+                 <Typography variant="h3" fontWeight="bold" mb="10px">
                     Contact Us
                  </Typography>
-                <Typography mb="30px">01 Special District of Yogyakarta, IDN 00045</Typography>
-                <Typography mb="30px">Email: yourcompanyemail@gmail.com</Typography>
-                <Typography mb="30px">(+62) 340 043 340</Typography>
+                <Typography ml={isNonMobile ? undefined : "5px"} mb="5px">Yogyakarta, IDN 00045</Typography>
+                <Typography ml={isNonMobile ? undefined : "5px"} mb="5px">Email@gmail.com</Typography>
+                <Typography ml={isNonMobile ? undefined : "5px"} mb="5px">(+62) 340 043 340</Typography>
               </Box>  
 
             </Box>
